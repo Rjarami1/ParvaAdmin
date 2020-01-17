bd9ca0c2e3831ab7ebb2dfa72a7d1140080c1d8d
Ricardo Jaramillo <Rjarami1@eafit.edu.co>
2020-01-16 23:10:03 -0500

    Añadiendo comunicación entre página y main Renderer

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const path = require('path');
 const url = require('url')
 
 //Initializing electron objects
-const {app, BrowserWindow, Menu, ipcMain} = electron
+const {app, BrowserWindow, Menu, ipcMain} = electron;
 
 //Declaring Browser Windows
 let mainWindow;
@@ -18,6 +18,56 @@ function createMainWindow() {
     })
 
     mainWindow.loadFile('mainWindow.html');
+
+    mainWindow.on('close', () => {
+        mainWindow = null;
+    })
 }
 
-app.on('ready', createMainWindow);
+app.on('ready', () => {
+    createMainWindow();
+
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+});
+
+ipcMain.on('login:in', (e, arr) => {
+    console.log(arr[0]);
+    console.log(arr[1]);
+})
+
+const mainMenuTemplate = [
+    {
+        label: 'Archivo',
+        submenu: [
+            {
+                label: 'Salir',
+                accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+                click(){
+                    app.quit();
+                }
+            },
+            {
+                label: 'Cerrar Sesión'
+            }
+        ]
+    }
+];
+
+if(process.env.NODE_ENV !== 'production'){
+    mainMenuTemplate.push({
+        label: 'Dev tools',
+        submenu:[
+            {
+                label: 'Activar',
+                accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+                click(item, focusedWindow){
+                    focusedWindow.webContents.openDevTools();
+                }
+        },
+        {
+            role: 'reload'
+        }
+    ]
+    })
+}
